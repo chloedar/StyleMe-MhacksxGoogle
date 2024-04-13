6b8bf2858238c47b3ca42f6581986f42bd4946aa
chloedar <chloedar@umich.edu>
2024-04-13 12:00:51 -0400

    hi

--- backend/gemini.py
+++ backend/gemini.py
@@ -6,13 +6,14 @@ def wardrobe(query, image_folder):
 
     model = genai.GenerativeModel('gemini-pro-vision')
     chat = model.start_chat(history=[])
+    query_str = ""+ query 
     resp_list = []
-
     resp_list.append(query)
 
     for image in image_folder:
         resp_list.append(image)
 
     response = model.generate_content(resp_list, stream=True)
-    
     response.resolve()
+
+    return to_markdown(response.text)
